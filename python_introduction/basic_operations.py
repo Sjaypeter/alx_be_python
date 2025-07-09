70c0c55a86b5b3fe618bcdcf719cff56cad2346e
Peter Saint-John <Sjaypeter.sjp@gmail.com>
2025-07-09 21:07:28 +0100

    operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1= 10
+number2 = 5
+x = number1 + number2
+y = number1 - number2
+z = number1 * number2
+print(f"sum of {number1} and {number2} is {x}")
+print(f"Difference of {number1} and {number2} is {y}")
+print(f"Product of {number1} and {number2} is {z}")
