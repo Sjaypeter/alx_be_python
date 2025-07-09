9d8de3b5a980ba178f31cb243befe0c1feea86ef
Peter Saint-John <Sjaypeter.sjp@gmail.com>
2025-07-09 21:27:08 +0100

    Resolved changes

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,8 +1,6 @@
+#simple interest calculator 
 principal = 1000
 rate = 0.05 
 time = 3
-P = principal
-R = rate
-T = time
-interest = P * R * T
+interest = principal * rate * time
 print(f"The simple interest is {interest}")
