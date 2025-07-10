ba980b11911f9b773e0e7e40dc7ff6d9e8c350ac
Peter Saint-John <Sjaypeter.sjp@gmail.com>
2025-07-10 11:10:10 +0100

    Age calculator

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,5 +1,5 @@
 #This script calculates age
-age = int(input("“How old are you? "))
+age = int(input("How old are you? "))
 #In year 2050 the user will be
 user_age = 27 + age
 print(f"In 2050 you will be {user_age} years old")
