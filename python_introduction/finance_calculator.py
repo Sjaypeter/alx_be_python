9adbc73c22ab4f187eb3331e635c1e502ae7df9f
Peter Saint-John <Sjaypeter.sjp@gmail.com>
2025-07-10 11:31:29 +0100

    Finance calculator

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,7 +1,7 @@
 #This script calculates savings
 #1. User input for financial details
-Monthly_income = int(input("Enter your monthly income:$"))
-Monthly_expenses = int(input("Enter your total monthly expenses:$"))
+Monthly_income = int(input("Enter your monthly income: "))
+Monthly_expenses = int(input("Enter your total monthly expenses: "))
 #2. Monthly savings 
 Monthly_savings = Monthly_income - Monthly_expenses
 #3. Project annual savings with an annual rate of 5%
