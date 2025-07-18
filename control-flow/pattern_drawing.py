735c5ec7baa9331a5cd55858f089f0fcd6582144
Peter Saint-John <Sjaypeter.sjp@gmail.com>
2025-07-18 19:16:38 +0100

    Pattern

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,9 +1,7 @@
 while True:
     size_of_pattern = int(input("Enter the size of the pattern: "))
-    symbol = input("Enter your symbol: ")
     for x in range(size_of_pattern):
         for y in range(size_of_pattern):
-            print(symbol, end=" ")
-        print()
+            print("*", end=" ")
 
         
