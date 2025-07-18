c8444df84450aa7db3c5790d1819f2075ccd461f
Peter Saint-John <Sjaypeter.sjp@gmail.com>
2025-07-18 17:48:12 +0100

    Pattern size

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,9 @@
+while True:
+    size_of_pattern = int(input("Enter the size of the pattern: "))
+    for x in range(size_of_pattern):
+        for y in range(size_of_pattern):
+            print("*", end=" ")
+        
+
+
+        
