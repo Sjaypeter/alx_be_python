a869e18b528ce8604c85f54c83320ec8b159f338
Peter Saint-John <Sjaypeter.sjp@gmail.com>
2025-07-20 10:49:26 +0100

    Daily-reminder

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,23 +1,20 @@
-#1.Prompt user for task description
-task = input("Enter your task: ")
-priority = input("Priority (high/medium/low): ")
-time_bound = input("Is it time bound? (yes/no): ")
-#2. Process case bsed on priority and time constraint
+#1.Prompt user for task
+task = input("Enter your task: ").lower()
+priority = input("Priority (high/medium/low): ").lower()
+time_bound = input("Is it time-bound? (yes/no): ").lower()
 match priority:
     case "high":
-        if time_bound == "yes":
-            print(f"Reminder: {task} is a high priority task that requires immediate attention today!")
-        else:
-            print("Take some time out but get back to it cause its of high priority")
+            reminder = f"Reminder:{task} is a high priority task"
     case "medium":
-        if time_bound == "yes":
-            print(f"Reminder: {task} is a medium priority task that requires attention today!")
-        else:
-            print(f"You still have some time to finsh {task}")
+            reminder = f"Reminder:{task} is a medium priority task"
     case "low":
-        if time_bound == "yes":
-            print(f"Reminder: {task} is of low  priority but still attend to it today!")
-        else:
-            print(f"Read a book {task} a low priority task. Consider completing it when you have free time.")
+            reminder = f"Reminder:{task} is a low priority task"
     case _:
-        print("Invalid response")
+        print("Invalid response. Please enter a task")
+if time_bound == "yes":
+    reminder += "that requires immediate attention today!"
+else:
+    reminder += "Consider completing it when you have free time."
+print("reminder:", reminder)
+        
+        
