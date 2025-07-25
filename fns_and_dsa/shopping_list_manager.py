9df57d2eac957239ef693be84ab9eb95f468b43e
Peter Saint-John <Sjaypeter.sjp@gmail.com>
2025-07-25 17:10:03 +0100

    Shopping cart

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -5,10 +5,10 @@ def display_menu():
     print("3. View List")
     print("4. Exit")
 def add_item():
-    item_name = input("Enter the item: ")
+    item_name = input("Enter the item to add: ")
     shopping_list.append(item_name)
 def remove_item():
-    item_name += input("Enter the name of the item")
+    item_name += input("Enter the item to remove: ")
     shopping_list.remove(item_name)
 def view_item():
     print(shopping_list)
@@ -16,7 +16,7 @@ shopping_list = []
 is_running = True
 while is_running:
     display_menu()
-    choice = (input("Enter your choice(1-4): "))
+    choice = (int(input("Enter your choice(1-4): ")))
     if choice == "1":
         add_item()
     elif choice == "2":
@@ -25,9 +25,9 @@ while is_running:
         remove_item()
     elif choice =="4":
         is_running = False
-        print("Thank you for shopping")
+        print("Goodbye")
     else:
-        print("Invalid choice")
+        print("Invalid choice. Please try again!")
 
         
     
