e8b44a7dada8898696e00ff2a1592d7edaf97c83
Peter Saint-John <Sjaypeter.sjp@gmail.com>
2025-08-02 15:01:20 +0100

    Paradigms

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,10 +1,12 @@
 def safe_divide(numerator, denominator):
     try:
-        numerator = input("Enter numerator: ")
-        denominator = input("Enter denominator: ")
+        numerator = float(input("Enter numerator: "))
+        denominator = float(input("Enter denominator: "))
     except ZeroDivisionError:
         print("Error: Cannot divide by zero.")
     except ValueError:
-        print("Error: Please enter numeric values only")
+        print("Error: Please enter numeric values only.")
     else:
-        return numerator/denominator
+        result = numerator/denominator
+        print(f"The result of the division is {result}")
+safe_divide(10,5)
