9c6aefe66696dc48dca14038d0cb3ec08de74fec
Peter Saint-John <Sjaypeter.sjp@gmail.com>
2025-08-02 22:05:13 +0100

    Paradigms

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,18 @@
+class Book:
+    def __init__(self,title,author,_is_checked_out):
+        self.title = title
+        self.author = author
+        self._is_checked_out = True
+class Library(Book):
+    
+        def 
+            
+        def add_book(self):
+            pass
+        def check_out_book(title):
+            pass
+        def return_book(title):
+            pass
+        def list_available_books(self):
+            pass
+                
