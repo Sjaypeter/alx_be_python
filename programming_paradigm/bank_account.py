16591f36a9c8bfdb87e0a588a52b95044f3430ec
Peter Saint-John <Sjaypeter.sjp@gmail.com>
2025-08-02 22:43:41 +0100

    Paradigms

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,14 +1,17 @@
 class BankAccount:
-    def __init__(self,initial_balance = 0):
+    def __init__(self, initial_balance=0):
         self.account_balance = float(initial_balance)
-    def deposit(self,amount):
+        
+    def deposit(self, amount):
         self.account_balance += amount
-    def withdraw(self,amount):
-        if self.account_balance > amount:
+        
+    def withdraw(self, amount):
+        if amount <= self.account_balance:
             self.account_balance -= amount
             return True
         else:
             return False
+        
     def display_balance(self):
         print(f"Current Balance: ${self.account_balance:.2f}")
         
