f2a8497e65f742e0b82e7a50632f4bc03f8febc7
Peter Saint-John <Sjaypeter.sjp@gmail.com>
2025-08-02 14:30:14 +0100

    Paradigms

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -7,10 +7,10 @@ class BankAccount:
     def withdraw(self,amount):
         if self.account_balance > amount:
             self.account_balance -= amount
-            print(f"Withdrew: {amount}")
+            print(f"Withdrew: ${amount}")
             return True
         else:
-            print(f"Insufficient funds")
+            print(f"Insufficient funds.")
             return False
     def display_balance(self):
         print(f"Current Balance: ${self.account_balance:.2f}")
