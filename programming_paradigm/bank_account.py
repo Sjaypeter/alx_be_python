846120a5406517ecdf5b16261b581cd4be0dc9cb
Peter Saint-John <Sjaypeter.sjp@gmail.com>
2025-08-02 22:09:56 +0100

    Paradigms

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -13,5 +13,5 @@ class BankAccount:
             print(f"Insufficient funds.")
             return False
     def display_balance(self):
-        print(f"Current Balance: ${self.account_balance:.2f}")
+        print(f"Current Balance: ${self.account_balance}")
         
