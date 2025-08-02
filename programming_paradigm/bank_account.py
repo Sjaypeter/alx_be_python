34e82ed5142a822c8970948090cdfa8d418761a2
Peter Saint-John <Sjaypeter.sjp@gmail.com>
2025-08-02 14:27:16 +0100

    Paradigms

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,17 @@
+class BankAccount:
+    def __init__(self,account_balance):
+        self.account_balance = account_balance
+        initial_balance = 0
+    def deposit(self,amount):
+        self.account_balance += amount
+    def withdraw(self,amount):
+        if self.account_balance > amount:
+            self.account_balance -= amount
+            print(f"Withdrew: {amount}")
+            return True
+        else:
+            print(f"Insufficient funds")
+            return False
+    def display_balance(self):
+        print(f"Current Balance: ${self.account_balance:.2f}")
+        
