c6a7b28a37355a7ce5e007e6133c7db42f0d137a
Peter Saint-John <Sjaypeter.sjp@gmail.com>
2025-08-03 18:51:47 +0100

    Paradigms

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -1,18 +1,45 @@
 class Book:
-    def __init__(self,title,author,_is_checked_out):
-        self.title = title
+    def __init__(self, author, title,):
         self.author = author
-        self._is_checked_out = True
-class Library(Book):
+        self.title = title
+        self.is_checked_out = False
+        
+    def check_out(self):
+        if not self.is_checked_out:
+            self.is_checked_out = True
+            return True
+        return False
     
-        def 
+    def return_book(self):
+        if self.is_checked_out:
+            self.is_checked_out = False
+            return True
+        return False
+    
+    def is_available(self):
+        return not self.is_checked_out
+    
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+    
+class Library:
+    
+    def __init__(self):
+        self._books = []
+        
+    def check_out_book(self,title):
+        for book in self._books:
+            if book.title == title and book.is_available():
+                book.check_out()
+                return
+    def return_book(self, title):
+        for book in self._books:
+            if book.title == title and not book.is_available:
+                book.return_book()
+                return
             
-        def add_book(self):
-            pass
-        def check_out_book(title):
-            pass
-        def return_book(title):
-            pass
-        def list_available_books(self):
-            pass
-                
+    def list_available_books(self):
+        for book in self._books:
+            if book.is_available():
+                print(book)
+        
