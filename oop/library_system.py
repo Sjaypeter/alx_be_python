7603741e7263c6290d48b74ee3dfbcc13b6b4500
Peter Saint-John <Sjaypeter.sjp@gmail.com>
2025-08-09 20:54:36 +0100

    oop

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,41 +1,37 @@
 class Book:
-    def __init__(self, title:str, author:str):
+    def __init__(self,title:str, author:str):
         self.title = title
         self.author = author
         
     def __str__(self):
-        return f"Book : {self.title} by {self.author}"
-    
-    def get_details(self):
-        return str(self)
+        return f"Book: {self.title} by {self.author}"
         
 class EBook(Book):
-    def __init__(self, title:str,author:str,file_size:int):
+    def __init__(self, title, author,file_size:int):
         super().__init__(title, author)
         self.file_size = file_size
         
     def __str__(self):
         return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
-        
+
 class PrintBook(Book):
-    def __init__(self, title:str, author: str,page_count:int):
-        super().__init__(title,author)
+    def __init__(self, title, author, page_count:int):
+        super().__init__(title, author)
         self.page_count = page_count
         
     def __str__(self):
         return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
-        
-    def get_details(self):
-        return str(self)
+
 class Library:
     def __init__(self):
         self.books = []
         
-    def add_book(self,book:Book):
+    def add_book(self, book):
         self.books.append(book)
         
     def list_books(self):
         for book in self.books:
             print(book)
+        
         
         
